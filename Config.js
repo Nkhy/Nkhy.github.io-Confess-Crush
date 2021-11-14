979ac89d1006c6aef1d791c25bea2f9b35805f7a
Nkhy <94232709+Nkhy@users.noreply.github.com>
2021-11-15 02:09:15 +0700

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,23 @@
+const CONFIG = {
+    titleWeb: "mời cô bạn cấp 2",
+    introTitle: 'nhi nhẻo',
+    introDesc: 'web mới tạo thấy sao đẹp chứ', 
+    btnIntro: '^^HiHi^^',
+    title: ' _   _ _     _ 
+| \ | | |   (_)
+|  \| | |__  _ 
+| . ` | '_ \| |
+| |\  | | | | |
+\_| \_/_| |_|_|
+🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'cảm ơn bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'cố lên❤️❤️❤️ <33333333',
+    mess: 'Cảm ơn bạn đã xem và bỏ thời gian quý báu của mình 😘😘',
+    messDesc: 'Tối nay nhớ ngủ sớm đi nha😊😊.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://github.com/nkhy/Còness-banthan' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
